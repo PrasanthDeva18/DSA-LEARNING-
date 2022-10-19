4c4e1d8421f837edbf861c1099d600a6a54e0774
PRASANTH DEVA <88628112+PrasanthDeva18@users.noreply.github.com>
2022-10-19 23:36:04 +0530

    Update Tricky.c

--- Rescursion/Tricky.c
+++ Rescursion/Tricky.c
@@ -17,3 +17,25 @@ int main(){
     printf("%d",func(5));
     return 0;
 }
+
+
+#include<stdio.h>
+int func(int n){
+    static int x=0;
+    if(n>0){
+        x++;
+        // for(int i=1;i<=n;i++)
+        //   printf("%d",i);
+         return func(n-1)+2;
+    //int a= func(n-1)+x;//555550
+    // for(int i=0;i>=func(n-1);i++)
+    //   printf("%d",i);
+    }
+    return 0;
+}
+int main(){
+  for(int i=1;i<=4;i++)
+    printf("%d\n",func(i));
+    // printf("\n");
+    return 0;
+}
